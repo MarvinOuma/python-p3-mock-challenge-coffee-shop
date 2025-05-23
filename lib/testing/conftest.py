e2e0c67ab901fb19ffb47adbd5cbcca6965ae6c4
MarvinDanielOduor <marvin.ouma@strathmore.edu>
2025-05-23 09:25:48 +0300

    conftest.py

--- lib/testing/conftest.py
+++ lib/testing/conftest.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 
+def customize_test_names(item):
+    parent_object = item.parent.obj
+    test_function = item.obj
+
+    # Extracting docstrings or names
+    parent_prefix = parent_object.__doc__.strip() if parent_object.__doc__ else parent_object.__class__.__name__
+    test_suffix = test_function.__doc__.strip() if test_function.__doc__ else test_function.__name__
+
+    # Constructing custom test name
+    custom_name = ' '.join((parent_prefix, test_suffix)) if parent_prefix or test_suffix else None
+
+    if custom_name:
+        item._nodeid = custom_name
+
+# Registering the function to customize test names
 def pytest_itemcollected(item):
-    par = item.parent.obj
-    node = item.obj
-    pref = par.__doc__.strip() if par.__doc__ else par.__class__.__name__
-    suf = node.__doc__.strip() if node.__doc__ else node.__name__
-    if pref or suf:
-        item._nodeid = ' '.join((pref, suf))
+    customize_test_names(item)
