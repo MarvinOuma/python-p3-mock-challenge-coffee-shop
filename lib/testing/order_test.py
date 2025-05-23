df7fde73bbaa9473cb1aa4d2eaa22063dca7e93c
MarvinDanielOduor <marvin.ouma@strathmore.edu>
2025-05-23 09:34:22 +0300

    order_test.py

--- lib/testing/order_test.py
+++ lib/testing/order_test.py
@@ -6,7 +6,14 @@ from classes.many_to_many import Order
 
 
 class TestOrders:
-    '''Order in many_to_many.py'''
+    '''Tests for the Order class'''
+
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        yield
+        Coffee.instances = []
+        Customer.instances = []
+        Order.instances = []
 
     def test_has_price(self):
         '''Order is initialized with a price'''
@@ -15,84 +22,79 @@ class TestOrders:
         order_1 = Order(customer, coffee, 2.0)
         order_2 = Order(customer, coffee, 5.0)
 
-        assert (order_1.price == 2.0)
-        assert (order_2.price == 5.0)
-    
+        assert order_1.price == 2.0
+        assert order_2.price == 5.0
+
     def test_price_is_valid(self):
-        """price is of type float and between 1.0 and 10.0"""
+        """Price is of type float and between 1.0 and 10.0"""
         coffee = Coffee("Mocha")
         customer = Customer('Steve')
         order_1 = Order(customer, coffee, 2.0)
         order_2 = Order(customer, coffee, 5.0)
-        
+
         assert isinstance(order_1.price, float)
         assert isinstance(order_2.price, float)
-        
-        # uncomment the next two lines if using Exceptions
-        # with pytest.raises(Exception):
-        #     Order(customer, coffee, 0.99)
-        
-        # uncomment the next two lines if using Exceptions
-        # with pytest.raises(Exception):
-        #     Order(customer, coffee, 10.01)
-        
+
+        # Check if price validation works
+        with pytest.raises(Exception):
+            Order(customer, coffee, 0.99)
+
+        with pytest.raises(Exception):
+            Order(customer, coffee, 10.01)
+
     def test_price_is_immutable(self):
-        """price is immutable"""
+        """Price is immutable"""
         coffee = Coffee("Mocha")
         customer = Customer('Steve')
         order_1 = Order(customer, coffee, 2.0)
-        
-        # comment out the next two lines if using Exceptions
+
+        # Check if price remains unchanged
         order_1.price = 3.0
         assert order_1.price == 2.0
-        
-        # uncomment the next two lines if using Exceptions
-        # with pytest.raises(Exception):
-        #     order_1.price = 3.0
 
     def test_has_a_customer(self):
-        '''order has a customer .'''
+        '''Order has a customer'''
         coffee = Coffee("Mocha")
         customer_1 = Customer('Wayne')
         customer_2 = Customer('Dima')
         order_1 = Order(customer_1, coffee, 2.0)
         order_2 = Order(customer_2, coffee, 5.0)
 
-        assert (order_1.customer == customer_1)
-        assert (order_2.customer == customer_2)
+        assert order_1.customer == customer_1
+        assert order_2.customer == customer_2
 
     def test_customer_of_type_customer(self):
-        '''customer is of type Customer'''
+        '''Customer is of type Customer'''
         coffee = Coffee("Vanilla Latte")
         customer_1 = Customer('Wayne')
         customer_2 = Customer('Dima')
         order_1 = Order(customer_1, coffee, 2.0)
         order_2 = Order(customer_2, coffee, 5.0)
 
-        assert (isinstance(order_1.customer, Customer))
-        assert (isinstance(order_2.customer, Customer))
+        assert isinstance(order_1.customer, Customer)
+        assert isinstance(order_2.customer, Customer)
 
     def test_has_a_coffee(self):
-        '''order has a coffee.'''
+        '''Order has a coffee'''
         coffee_1 = Coffee("Mocha")
         coffee_2 = Coffee("Peppermint Chai")
         customer = Customer('Wayne')
         order_1 = Order(customer, coffee_1, 2.0)
         order_2 = Order(customer, coffee_2, 5.0)
 
-        assert (order_1.coffee == coffee_1)
-        assert (order_2.coffee == coffee_2)
+        assert order_1.coffee == coffee_1
+        assert order_2.coffee == coffee_2
 
     def test_coffee_of_type_coffee(self):
-        '''coffee is of type Coffee'''
+        '''Coffee is of type Coffee'''
         coffee_1 = Coffee("Vanilla Latte")
         coffee_2 = Coffee("Peppermint Chai")
         customer = Customer('Steve')
         order_1 = Order(customer, coffee_1, 2.0)
         order_2 = Order(customer, coffee_2, 5.0)
 
-        assert (isinstance(order_1.coffee, Coffee))
-        assert (isinstance(order_2.coffee, Coffee))
+        assert isinstance(order_1.coffee, Coffee)
+        assert isinstance(order_2.coffee, Coffee)
 
     def test_get_all_orders(self):
         '''Order class all attribute'''
@@ -103,6 +105,6 @@ class TestOrders:
         order_1 = Order(customer, coffee, 2.0)
         order_2 = Order(customer_2, coffee, 5.0)
 
-        assert (len(Order.all) == 2.0)
-        assert (order_1 in Order.all)
-        assert (order_2 in Order.all)
+        assert len(Order.all) == 2
+        assert order_1 in Order.all
+        assert order_2 in Order.all
